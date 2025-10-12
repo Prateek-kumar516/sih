e496ead29a5e02c3107979677efbf31403163627
Prateek-kumar516 <prateekkumarr516@gmail.com>
2025-10-12 17:52:19 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext";
+import { AuthProvider } from "./context/Authcontext";
 import LandingPage from "./pages/LandingPage";
 import Login from "./pages/Login";
 import StudentDashboard from "./components/StudentDashboard";
